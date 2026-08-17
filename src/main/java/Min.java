40792773182f79d9566104a6f3c1a5f0c0ba4391
L-TM <41814626+L-TM@users.noreply.github.com>
2026-08-17 13:38:07 +0800

    Echo user commands until bye

--- src/main/java/Min.java
+++ src/main/java/Min.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Min {
-    private static final int SEPARATOR_LENGTH = 50;
-    private static final String SEPARATOR = "-".repeat(SEPARATOR_LENGTH);
+    private static final int SEPARATOR_LENGTH = 60;
+    private static final String SEPARATOR = "_".repeat(SEPARATOR_LENGTH);
+    private static final String EXIT_COMMAND = "bye";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String banner = " __  __ _       \n"
                 + "|  \\/  (_)_ __  \n"
                 + "| |\\/| | | '_ \\ \n"
@@ -13,7 +17,19 @@ public class Min {
         System.out.println("Hello! I'm Min.");
         System.out.println("What can I do for you?");
         System.out.println(SEPARATOR);
-        System.out.println("Bye. Hope you will have some fun!");
-        System.out.println(SEPARATOR);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(SEPARATOR);
+
+            if (command.equals(EXIT_COMMAND)) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(SEPARATOR);
+                break;
+            }
+
+            System.out.println("\t" + command);
+            System.out.println(SEPARATOR);
+        }
     }
 }
