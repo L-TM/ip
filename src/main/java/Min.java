efcc2102e55d2b0590ec00624c5be0d223b06667
L-TM <41814626+L-TM@users.noreply.github.com>
2026-08-17 13:58:28 +0800

    Store and list user tasks

--- src/main/java/Min.java
+++ src/main/java/Min.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 public class Min {
     private static final int SEPARATOR_LENGTH = 60;
+    private static final int MAX_TASKS = 100;
     private static final String SEPARATOR = "_".repeat(SEPARATOR_LENGTH);
     private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
         String banner = " __  __ _       \n"
                 + "|  \\/  (_)_ __  \n"
                 + "| |\\/| | | '_ \\ \n"
@@ -26,10 +31,17 @@ public class Min {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(SEPARATOR);
                 break;
+            } else if (command.equals(LIST_COMMAND)) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(SEPARATOR);
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
+                System.out.println(SEPARATOR);
             }
-
-            System.out.println("\t" + command);
-            System.out.println(SEPARATOR);
         }
     }
 }
