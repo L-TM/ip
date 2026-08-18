6e601cbc0752bc80618809772d9f23ecff8be4fc
L-TM <41814626+L-TM@users.noreply.github.com>
2026-08-18 10:47:47 +0800

    Add task marking/unmarking commands
    Let's store tasks as Task objects and support marking and unmarking
    tasks by their list number

--- src/main/java/Min.java
+++ src/main/java/Min.java
@@ -4,12 +4,15 @@ public class Min {
     private static final int SEPARATOR_LENGTH = 60;
     private static final int MAX_TASKS = 100;
     private static final String SEPARATOR = "_".repeat(SEPARATOR_LENGTH);
+
     private static final String EXIT_COMMAND = "bye";
     private static final String LIST_COMMAND = "list";
+    private static final String MARK_COMMAND = "mark ";
+    private static final String UNMARK_COMMAND = "unmark ";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
         String banner = " __  __ _       \n"
@@ -32,12 +35,28 @@ public class Min {
                 System.out.println(SEPARATOR);
                 break;
             } else if (command.equals(LIST_COMMAND)) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(SEPARATOR);
+            } else if (command.startsWith(MARK_COMMAND)) {
+                // Handle mark command
+                String[] parts = command.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]);
+                tasks[taskNumber - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" " + tasks[taskNumber - 1]);
+                System.out.println(SEPARATOR);
+            } else if (command.startsWith(UNMARK_COMMAND)) {
+                String[] parts = command.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]);
+                tasks[taskNumber - 1].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(" " + tasks[taskNumber - 1]);
+                System.out.println(SEPARATOR);
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println(" added: " + command);
                 System.out.println(SEPARATOR);
