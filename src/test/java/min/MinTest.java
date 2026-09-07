bd35f9d7467bea9caa1aa16146105fa3b3c2ea9e
L-TM <41814626+L-TM@users.noreply.github.com>
2026-09-07 19:42:24 +0800

    Min: Extract command handlers
    executeCommand handles dispatch, parsing, task mutation, persistence,
    and response formatting in one method. This makes command dispatch
    harder to scan and gives the method several reasons to change.
    
    Extract the detailed find, mark, unmark, and delete branches into
    focused helpers so the dispatcher stays at one level of abstraction.
    Add characterization tests to preserve command responses and save
    behavior.

--- src/test/java/min/MinTest.java
+++ src/test/java/min/MinTest.java
@@ -16,7 +16,7 @@ import min.task.Todo;
 
 class MinTest {
     private final Parser parser = new Parser();
-    private final Storage storage = new NoOpStorage();
+    private final RecordingStorage storage = new RecordingStorage();
 
     @Test
     void isExitCommand_byeWithSurroundingWhitespace_returnsTrue() {
@@ -39,6 +39,120 @@ class MinTest {
         Min min = new Min(parser, new TaskList(List.of()), storage);
 
         assertEquals(" Bye. Hope to see you again soon!", min.getResponse("bye"));
+    }
+
+    @Test
+    void getResponse_todo_addsTaskAndReturnsConfirmation() {
+        TaskList tasks = new TaskList(List.of());
+        Min min = new Min(parser, tasks, storage);
+
+        String response = min.getResponse("todo read book");
+
+        assertEquals(" Got it. I've added this task:\n"
+                + "   [T][ ] read book\n"
+                + " Now you have 1 tasks in the list.", response);
+        assertEquals("T | 0 | read book", tasks.getTasks().get(0).toFileString());
+        assertEquals(1, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_deadline_addsTaskAndReturnsConfirmation() {
+        TaskList tasks = new TaskList(List.of());
+        Min min = new Min(parser, tasks, storage);
+
+        String response = min.getResponse("deadline submit report /by 2026-08-28");
+
+        assertEquals(" Got it. I've added this task:\n"
+                + "   [D][ ] submit report (by: Aug 28 2026)\n"
+                + " Now you have 1 tasks in the list.", response);
+        assertEquals("D | 0 | submit report | 2026-08-28",
+                tasks.getTasks().get(0).toFileString());
+        assertEquals(1, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_event_addsTaskAndReturnsConfirmation() {
+        TaskList tasks = new TaskList(List.of());
+        Min min = new Min(parser, tasks, storage);
+
+        String response = min.getResponse("event meeting /from 2pm /to 4pm");
+
+        assertEquals(" Got it. I've added this task:\n"
+                + "   [E][ ] meeting (from: 2pm to: 4pm)\n"
+                + " Now you have 1 tasks in the list.", response);
+        assertEquals("E | 0 | meeting | 2pm | 4pm",
+                tasks.getTasks().get(0).toFileString());
+        assertEquals(1, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_mark_marksTaskAndReturnsConfirmation() {
+        Todo task = new Todo("read book");
+        Min min = new Min(parser, new TaskList(List.of(task)), storage);
+
+        String response = min.getResponse("mark 1");
+
+        assertEquals("Nice! I've marked this task as done:\n"
+                + "   [T][X] read book", response);
+        assertTrue(task.isDone());
+        assertEquals(1, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_unmark_unmarksTaskAndReturnsConfirmation() {
+        Todo task = new Todo("read book");
+        task.markAsDone();
+        Min min = new Min(parser, new TaskList(List.of(task)), storage);
+
+        String response = min.getResponse("unmark 1");
+
+        assertEquals("OK, I've marked this task as not done yet:\n"
+                + "   [T][ ] read book", response);
+        assertFalse(task.isDone());
+        assertEquals(1, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_delete_removesTaskAndReturnsConfirmation() {
+        Todo deletedTask = new Todo("read book");
+        Todo remainingTask = new Todo("buy milk");
+        TaskList tasks = new TaskList(List.of(deletedTask, remainingTask));
+        Min min = new Min(parser, tasks, storage);
+
+        String response = min.getResponse("delete 1");
+
+        assertEquals(" Got it. I've removed this task:\n"
+                + "   [T][ ] read book\n"
+                + " Now you have 1 tasks in the list.", response);
+        assertEquals(List.of(remainingTask), tasks.getTasks());
+        assertEquals(1, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_list_returnsNumberedTasks() {
+        TaskList tasks = new TaskList(List.of(
+                new Todo("read book"), new Todo("buy milk")));
+        Min min = new Min(parser, tasks, storage);
+
+        String response = min.getResponse("list");
+
+        assertEquals("Here are the tasks in your list:\n"
+                + " 1.[T][ ] read book\n"
+                + " 2.[T][ ] buy milk", response);
+        assertEquals(0, storage.getSaveCount());
+    }
+
+    @Test
+    void getResponse_find_returnsNumberedMatches() {
+        TaskList tasks = new TaskList(List.of(
+                new Todo("read book"), new Todo("buy milk")));
+        Min min = new Min(parser, tasks, storage);
+
+        String response = min.getResponse("find read");
+
+        assertEquals("Here are the matching tasks in your list:\n"
+                + " 1.[T][ ] read book", response);
+        assertEquals(0, storage.getSaveCount());
     }
 
     @Test
@@ -87,9 +201,16 @@ class MinTest {
         assertFalse(secondMatch.isDone());
     }
 
-    private static class NoOpStorage extends Storage {
+    private static class RecordingStorage extends Storage {
+        private int saveCount;
+
         @Override
         public void save(List<Task> tasks) {
+            saveCount++;
+        }
+
+        private int getSaveCount() {
+            return saveCount;
         }
     }
 }
